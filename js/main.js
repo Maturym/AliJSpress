4a41ffdcab8e66400009f27c5ffa8b2962a6962e
Elizabeth <whosingsin@gmail.com>
2020-11-10 22:33:51 +0300

    Day 2

--- js/main.js
+++ js/main.js
@@ -4,16 +4,35 @@ const search = document.querySelector('.search'),
   wishlistBtn = document.querySelector('#wishlist'),
   goodsWrapper = document.querySelector('.goods-wrapper'),
   cart = document.querySelector('.cart'),
-  cartClose = document.querySelector('.cart-close');
+  cartClose = document.querySelector('.cart-close'),
+  category = document.querySelector('.category');
 
-const createCardGoods = (id, price, title, img) => {
+
+const getGoods = (handler, filter) => {
+
+  fetch('db/db.json')
+      .then(resp => {
+        
+        if (resp.ok) {
+          return resp.json()
+        } else {
+          new Error(resp.status)
+        }
+      })
+      .then(filter)
+      .then(handler);
+
+};
+
+
+const createCardGoods = ({ id, price, title, imgMin: img }) => {
 
 
   const card = `
     <div class="card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3">
       <div class="card">
         <div class="card-img-wrapper">
-          <img class="card-img-top" src="img/temp/${img}" alt="">
+          <img class="card-img-top" src="${img}" alt="">
           <button class="card-add-wishlist"
             data-goods-id=${id}></button>
         </div>
@@ -33,21 +52,55 @@ const createCardGoods = (id, price, title, img) => {
 
 };
 
-const openCart = () => {
+const openCart = (e) => {
 
+  e.preventDefault();
   cart.style.display = 'flex';
 
+  document.addEventListener('keydown', closeCart);
 };
 
 
 const closeCart = (e) => {
   target = e.target;
+
+  if (target === cart || 
+    target === cartClose ||
+    e.code === 'Escape'){
+
+      cart.style.display = '';
+      document.removeEventListener('keydown', closeCart);
+  };
+};
+
+
+const renderCard = (item) => {
+  goodsWrapper.textContent = '';
+
+  item.forEach((item) => {
+    createCardGoods(item)
+  })
+
+};
+
+const randomSort = (item) => {  
+
+  return item.sort(() => (Math.random() - 0.5))
+
+};
+
+const chooseCategory = (e) => {
+  const target = e.target;
   e.preventDefault();
 
-  if (target === cart || target === cartClose){
+  if (target.classList.contains('category-item')) {
 
-    cart.style.display = '';
-  };
+    const category = target.dataset.category;
+
+    getGoods(renderCard,
+      goods => goods.filter(item => item.category.includes(category)))
+
+  }
 
 };
 
@@ -56,14 +109,8 @@ const closeCart = (e) => {
 cartBtn.addEventListener('click', openCart);
 cartClose.addEventListener('click', closeCart);
 cart.addEventListener('click', closeCart);
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'Escape') {
-    cart.style.display = '';
-  }
-});
+category.addEventListener('click', chooseCategory)
 
 
 
-createCardGoods(1, 2000, 'Дартс', 'archer.jpg');
-createCardGoods(2, 1500, 'Фламинго', 'flamingo.jpg');
-createCardGoods(3, 200, 'Носки', 'socks.jpg');
+getGoods(renderCard, randomSort);
